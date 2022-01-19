0674e08872c18d1a5f092552e81ae659c1686a21
raniiee <94891696+raniiee@users.noreply.github.com>
2022-01-19 14:57:10 +0530

    Update hello.go

--- kuber-demo/go_kubernetes/main/hello.go
+++ kuber-demo/go_kubernetes/main/hello.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-)
+    "fmt"
+    )
 
 func main() {
-	handler := http.NewServeMux()
-	handler.HandleFunc("/api/hello", Hello)
-	http.ListenAndServe("0.0.0.0:8080", handler)
-	// Listen to all request to port 8080.
-}
+    fmt.Println("Hello World")
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `Hello world`)
 }
 
